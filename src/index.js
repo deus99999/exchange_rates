31600ef47a6c9e0cd2f490f2346f6e66a039cf2e
deus99999 <rudenkooleksei@gmail.com>
2023-03-22 13:24:50 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,72 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './style.css'
+import ReactDOM from 'react-dom';
+import Rates from "./components/Rates";
+import Form from "./components/form";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const API_KEY = "uhKXWptEKch15TMo5Od6HVRmYPb9s3fB"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function ButtonUahUsd(props) {
+    return(<button type="button" id="UAH_buy_cell" className="btn btn-warning">
+        &#8372;
+        {" " + props.price_cell + " "}
+         /
+        {" " + props.price_buy}
+    </button>)
+}
+/*
+root.render(<ButtonUahUsd price_cell="36.5686" price_buy="40"/>);
+*/
+
+
+/*function Clock(props) {
+    return (
+        <div>
+            <h2>It's {props.date.toLocaleTimeString()}.</h2>
+        </div>
+    );
+}
+function tick() {
+    root.render(<Clock date={new Date()} />);
+}*/
+
+/*
+ticksetInterval(tick, 1000);
+*/
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick,
+            1000
+        );
+    }
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+    render() {
+        return (
+            <div>
+                <h2>Сейчас {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
+}
+
+/*root.render(<Clock />);*/
+
+
+root.render(<App />, document.getElementById('root'));
